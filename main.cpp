ff637c9408d72c2d9ad7c880dbc3058f9caa28e1
pyc6eh4uk <ruslan.klimov1@yandex.ru>
2018-03-21 17:39:04 +0300

    Input stdin

--- main.cpp
+++ main.cpp
@@ -8,16 +8,25 @@ int main() {
     std::vector<linear_cut_task_t::package_t> packages;
     std::vector<linear_cut_task_t::package_t> patterns;
 
-    packages.push_back({56, 0, 19});
-    packages.push_back({59, 0, 19});
-    packages.push_back({63, 0, 37});
+    freopen("input", "r", stdin);
 
-    patterns.push_back({6, 20, 38});
-    patterns.push_back({9, 40, 47});
-    patterns.push_back({12, 30, 32});
-    patterns.push_back({13, 50, 52});
-    patterns.push_back({21, 30, 46});
-    patterns.push_back({27, 50, 65});
+    int m, n, piece, length, count, low_length, high_length;
+    std::cin >> m;
+    std::cin >> n;
+    std::cin >> piece;
+
+    for (int i = 0; i < m; i++) {
+        std::cin >> length;
+        std::cin >> low_length;
+        std::cin >> high_length;
+        patterns.push_back({length, low_length, high_length});
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> length;
+        std::cin >> count;
+        packages.push_back({length, piece, count});
+    }
 
     auto task = new linear_cut_task_t(packages, patterns);
     //auto task = linear_cut_task_t::make_standart_simplex_task(packages, patterns);
